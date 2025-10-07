c65a9fdd633cecb3115d1f297b465bf9852051fe
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-10-07 12:25:57 -0500

    feat: install Go dependencies in build script

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ rm -rf dist/
 # Crea directorio
 mkdir -p dist/
 
+# Install dependencies
+go mod download
+go mod tidy
+
 # Linux AMD64
 GOOS=linux GOARCH=amd64 go build -o dist/db-migration-cli_linux_amd64 main.go
 
@@ -18,5 +22,5 @@ GOOS=darwin GOARCH=arm64 go build -o dist/db-migration-cli_darwin_arm64 main.go
 # Windows AMD64
 GOOS=windows GOARCH=amd64 go build -o dist/db-migration-cli_windows_amd64.exe main.go
 
-# Opcional: Comprime con UPX para reducir tamaño (instala UPX primero)
+
 upx --best dist/*
